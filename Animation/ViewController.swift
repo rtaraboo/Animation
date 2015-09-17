ef0d1d302b00fce2a95037dd0f459ae763568e97
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-09-17 15:49:34 -0400

    Initial Commit

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -9,15 +9,90 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var counter = 0
+    
+    var timer = NSTimer()
+    
+    var isAnimating = true
 
+
+    @IBOutlet var alienImage: UIImageView!
+    
+    
+    @IBAction func updateImage(sender: AnyObject) {
+        
+        if isAnimating == true {
+            
+            timer.invalidate()
+            
+            isAnimating = false
+        }
+        
+        else {
+            
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+            
+            isAnimating = true
+            
+        }
+        
+        
+
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+        
+        
+        
+        
     }
+    
+    func doAnimation() {
+        
+        
+        
+        if counter == 4 {
+            
+            counter = 1
+        }
+            
+        else {
+            
+            counter++ }
+        
+        
+        
+        alienImage.image = UIImage(named: "frame\(counter).png")
+        
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        alienImage.center = CGPointMake(alienImage.center.x - 400, alienImage.center.y)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        
+        UIView.animateWithDuration(1, animations: { () -> Void in
+            
+            self.alienImage.center = CGPointMake(self.alienImage.center.x + 400, self.alienImage.center.y)
+            
+            })
     }
 
 
